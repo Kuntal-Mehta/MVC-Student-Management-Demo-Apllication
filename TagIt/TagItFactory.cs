c585f78b43ac41b066ffb542cd6295367e73f934
Kuntal <ksharedrive@gmail.com>
2014-08-14 10:23:03 +0530

    TagIt

---
+++ TagIt/TagItFactory.cs
@@ -0,0 +1,46 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace TagIt
+{
+    #region Adornment Factory
+    /// <summary>
+    /// Establishes an <see cref="IAdornmentLayer"/> to place the adornment on and exports the <see cref="IWpfTextViewCreationListener"/>
+    /// that instantiates the adornment on the event of a <see cref="IWpfTextView"/>'s creation
+    /// </summary>
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    internal sealed class PurpleBoxAdornmentFactory : IWpfTextViewCreationListener
+    {
+        /// <summary>
+        /// Defines the adornment layer for the scarlet adornment. This layer is ordered 
+        /// after the selection layer in the Z-order
+        /// </summary>
+        [Export(typeof(AdornmentLayerDefinition))]
+        [Name("TagIt")]
+        [Order(After = PredefinedAdornmentLayers.Caret, Before=PredefinedAdornmentLayers.Text)]
+        [TextViewRole(PredefinedTextViewRoles.Document)]
+        public AdornmentLayerDefinition editorAdornmentLayer = null;
+        
+        //This import gives dte object of other instance of visual studio through which we can get the control of that instance.
+        [Import]
+        internal SVsServiceProvider ServiceProvider = null;
+        
+        /// <summary>
+        /// Instantiates a TagIt manager when a textView is created.
+        /// </summary>
+        /// <param name="textView">The <see cref="IWpfTextView"/> upon which the adornment should be placed</param>
+        public void TextViewCreated(IWpfTextView textView)
+        {
+            DTE2 dte = (DTE2)ServiceProvider.GetService(typeof(DTE));
+            new TagIt(textView,dte);
+            
+        }
+    }
+    #endregion //Adornment Factory
+}
